f49dbd1d1cdf551284a8ffacc6c951f8c59c5a1d
osho81 <ossi.hour@gmail.com>
2023-05-16 12:17:48 +0200

    Update comments

--- src/main/java/com/osho/carrental/CarRentalApplication.java
+++ src/main/java/com/osho/carrental/CarRentalApplication.java
@@ -8,9 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-
-// Comment to test PR triggered CI-based analysis; testing coverage
-
 @EnableEurekaClient
 @SpringBootApplication
 public class CarRentalApplication {
